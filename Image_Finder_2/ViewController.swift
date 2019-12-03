166e5a2fd47215eeda204dc7045d5307d50bcba8
Alexander Smith <alexsswift@gmail.com>
2019-12-02 16:32:42 -0800

    Initial Commit

---
+++ Image_Finder_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image_Finder_2
+//
+//  Created by Alexander Smith on 12/2/19.
+//  Copyright © 2019 Alexander Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
